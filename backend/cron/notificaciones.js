b518a9a2de6edf3a50cbcacc83f91e730525be12
Soporte Macktronica <soportemacktronica@gmail.com>
2025-08-25 16:33:25 -0600

    fix mensaje vencimiento a 2 dias¿

--- backend/cron/notificaciones.js
+++ backend/cron/notificaciones.js
@@ -1,33 +1,43 @@
 const schedule = require('node-schedule');
 const { getSock } = require('../utils/baileys');
 const { vencetredias } = require('../controllers/contrato.controller');
+const configuracion = require("../models/configuracion");
 
 
-schedule.scheduleJob('0 0 10 * * *', async () => {
+schedule.scheduleJob('0 0 8 * * *', async () => {
   const fecha = new Date();
-  fecha.setDate(fecha.getDate()+3);
+  fecha.setDate(fecha.getDate() + 3);
   const fechaformateada = fromatearfecha(fecha);
   const sock = getSock(); // ✅ obtener sock actual
   if (!sock) {
     console.log('⏳ Sock aún no está listo');
     return;
   }
+  const resconfig = await configuracion.findOne();
+  const msj = resconfig.vencimiento3Dias;
   const res = await vencetredias(fechaformateada);
-  console.log(res)
-  res.map(async (contrato) => {
-    const numero = '521'+contrato['persona.telefono'];
+  // Helper para pausar
+  const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+  for (const contrato of res) {
+    const numero = '521' + contrato['persona.telefono'];
     try {
       await sock.sendMessage(`${numero}@s.whatsapp.net`, {
-        text: 'tu renta vence en tres dias'
+        text: msj
       });
+      console.log(`✅ Mensaje enviado a ${numero}`);
+
+      // Espera 2 segundos entre cada envío
+      await sleep(2000);
     } catch (err) {
-      console.error('❌ Error al enviar documento:', err);
+      console.error('❌ Error al enviar mensaje:', err);
     }
-  })
- 
+  }
+
+
 });
 const fromatearfecha = (fecha) => {
   const fechaCompleta = new Date(fecha);
-const soloFecha = fechaCompleta.toISOString().split('T')[0];
-return soloFecha
+  const soloFecha = fechaCompleta.toISOString().split('T')[0];
+  return soloFecha
 }
