0d2ba83b13c9b645c17a321bf1ed1234ecfdd2f9
Soporte Macktronica <soportemacktronica@gmail.com>
2025-08-26 12:29:35 -0600

    fix descarga contrato vps

--- backend/controllers/documentos.controller.js
+++ backend/controllers/documentos.controller.js
@@ -575,7 +575,7 @@ const generarContrato = async (idContrato) => {
   </div>
 `;
     // Lanzar Puppeteer para generar PDF desde HTML
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({  args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']});
     const page = await browser.newPage();
 
     await page.setContent(html, { waitUntil: 'networkidle0' });
